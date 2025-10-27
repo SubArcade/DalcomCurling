9801972c4a319973a2218c6c2568c9bba756f20a
alenvoxx <light1014@naver.com>
2025-10-27 11:37:04 +0900

    [update] firebase signup, login

---
+++ Assets/_Project/Scripts/Firebase/FirebaseAuthManager.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase.Auth;
+using TMPro;
+using System;
+
+public class FirebaseAuthManager
+{
+    private static FirebaseAuthManager instance = null;
+    public static FirebaseAuthManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new FirebaseAuthManager();
+            }
+            return instance;
+        }
+    }
+    private FirebaseAuth auth; //로그인 / 회원가입
+    private FirebaseUser user; // 인증된 유저정보
+
+    public string UserId => user.UserId;
+
+    public Action<bool> LoginState;
+
+    
+    public void Init()
+    {
+        auth = FirebaseAuth.DefaultInstance;
+
+        if (auth.CurrentUser != null)
+        {
+            Logout();
+        }
+        auth.StateChanged += Onchanged;
+    }
+
+    private void Onchanged(object sender, EventArgs e)
+    {
+        if (auth.CurrentUser != null)
+        {
+            bool signed = (auth.CurrentUser != user && auth.CurrentUser != null);
+            if (!signed && user != null)
+            {
+                Debug.Log("로그아웃");
+                LoginState?.Invoke(false);
+            }
+
+            user = auth.CurrentUser;
+            if (signed)
+            {
+                Debug.Log("로그인");
+                LoginState?.Invoke(true);
+            }
+        }
+    }
+
+    public void Create(string email, string password)
+    {
+        auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWith(task =>
+        {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("회원가입 취소");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("회원가입 실패");
+                return;
+            }
+            FirebaseUser newUser = task.Result.User;
+            Debug.LogError("회원가입 완료");
+        });
+    }
+
+    public void Login(string email, string password)
+    {
+        auth.SignInWithEmailAndPasswordAsync(email, password).ContinueWith(task =>
+        {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("로그인 취소");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("로그인 실패");
+                return;
+            }
+            
+            FirebaseUser newUser = task.Result.User;
+            Debug.LogError("로그인 완료");
+        });
+    }
+
+    public void Logout()
+    {
+        auth.SignOut();
+        Debug.Log("로그아웃");
+    }
+
+
+}
---
+++ Assets/_Project/Scripts/Firebase/LoginSystem.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LoginSystem : MonoBehaviour
+{
+    public TMP_InputField email;
+    public TMP_InputField password;
+
+    public TMP_Text outputText;
+
+    void Start()
+    {
+        FirebaseAuthManager.Instance.LoginState += OnChangedState;
+        FirebaseAuthManager.Instance.Init();
+    }
+
+    private void OnChangedState(bool sign)
+    {
+        outputText.text = sign ? "Login : " : "Logout : ";
+        outputText.text += FirebaseAuthManager.Instance.UserId;
+    }
+
+    public void Create()
+    {
+        string e = email.text;
+        string p = password.text;
+
+        FirebaseAuthManager.Instance.Create(e, p);
+    }
+
+    public void Login()
+    {
+        FirebaseAuthManager.Instance.Create(email.text, password.text);
+    }
+
+    public void LogOut()
+    {
+        FirebaseAuthManager.Instance.Logout();
+    }
+
+}
