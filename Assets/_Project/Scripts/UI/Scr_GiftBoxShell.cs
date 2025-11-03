2edf5c07dbadd84980bf2eefc7faa917bb970c0b
yybbuu1 <shot135@naver.com>
2025-11-03 11:15:35 +0900

    메인화면 Pop창 제작
    * 파이어베이스, 충돌관리매니저
    
    * [update]파이어베이스 값 변경
    
    * [update]플레이어 레벨업 값 변경
    
    * [update]giftbox 수정중
    
    * [remove][fix] 폰트 수정 및 제거
    
    * [feat]UI 인스펙터 자동연결 스크립트 추가, 기프트박스 프리팹 생성
    
    [add]폰트 추가, dotween애니메이션용 스크립트 추가, 씬에 업그레이드,도감,플레이어레벨,기프트박스 팝업 추가
    
    ---------
    
    Co-authored-by: yybbuu1 <ybu1357@gmail.com>

---
+++ Assets/_Project/Scripts/UI/Scr_GiftBoxShell.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Localization.Plugins.XLIFF.V12;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scr_GiftBoxShell : MonoBehaviour
+{
+    [Header("기프트박스 버튼")]
+    [SerializeField] private Button giftBoxButton;
+
+    [Header("보상 수령 버튼")]
+    [SerializeField] private Button getRewardButton;
+
+    [Header("보상 이미지")]
+    [SerializeField] private Image giftBoxImage;
+    
+    private bool isActive = false;
+    private Scr_GiftBoxPopUp giftBoxPopUp;
+
+    void Awake()
+    {
+        //인스펙터 자동연결 싹다 다시해야합니다
+        giftBoxButton = transform.Find("Giftbox")?.GetComponent<Button>();
+        getRewardButton = transform.Find("GetRewardButton")?.GetComponentInChildren<Button>();
+        giftBoxImage = transform.Find("Giftbox")?.GetComponent<Image>();
+
+        giftBoxPopUp = FindObjectOfType<Scr_GiftBoxPopUp>();
+    }
+    void Start()
+    {
+        giftBoxPopUp = FindObjectOfType<Scr_GiftBoxPopUp>();
+        giftBoxButton.onClick.AddListener(OnClickGiftBox);
+        getRewardButton.onClick.AddListener(OnClickRewardButton);
+    }
+
+    void OnClickGiftBox()
+    {
+        isActive = !isActive;
+        giftBoxImage.color = isActive ? new Color(0, 0, 1, 0.3f) : Color.white;
+    }
+
+    void OnClickRewardButton()
+    {
+        giftBoxPopUp.OnClickOpenRewardButton(); // 중앙 팝업 호출
+        Destroy(this.gameObject);  // 셀 자체 제거
+    }
+}
---
+++ Assets/_Project/Scripts/UI/Scr_Main_Panel.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scr_Main_Panel : MonoBehaviour
+{
+    [Header("도감 팝업창 연결")]
+    [SerializeField] private GameObject DonutCodex;
+
+    [Header("도감 업그레이드 팝업창 연결")]
+    [SerializeField] private GameObject DonutUpgrade;
+
+    [Header("플레이어 레벨 팝업창 연결")]
+    [SerializeField] private GameObject PlayerLevelInfo;
+
+    [Header("기프트박스 팝업창 연결")]
+    [SerializeField] private GameObject GiftBoxPopUp;
+    // 11.03 작성날 기준 메인패널에 기프트박스 팝업을 띄울 버튼이나 이미지가 없음
+
+    [Header("각각 해당하는 팝업창 열기")]
+    [SerializeField] private Button EntryButton; //미완성  11.03
+    [SerializeField] private Button CodexButton;
+    [SerializeField] private Button basketButton; //미완성 11.03
+    [SerializeField] private Button UpgradeButton;
+    [SerializeField] private Button LevelButton;
+
+
+    void Awake()
+    {
+        Transform canvas = GameObject.Find("Canvas")?.transform;
+        DonutCodex = canvas.Find("DonutCodex")?.gameObject;
+        DonutUpgrade = canvas.Find("DonutUpgrade")?.gameObject;
+        PlayerLevelInfo = canvas.Find("PlayerLevelInfo")?.gameObject;
+        GiftBoxPopUp = canvas.Find("GiftBoxPopUp")?.gameObject;
+
+        EntryButton = transform.Find("Bottom/ButtonGroup/Entry_Button")?.GetComponent<Button>();
+        //엔트리버튼 연결만 해놓고 팝업창은 따로 만들어야함
+        CodexButton = transform.Find("Bottom/ButtonGroup/Codex_Button")?.GetComponent<Button>();       
+        basketButton = transform.Find("Bottom/ButtonGroup/basket_Button")?.GetComponent<Button>();
+        //휴지통버튼 연결만 해놓고 팝업차은 따로 만들어야함
+        UpgradeButton = transform.Find("Bottom/ButtonGroup/Upgrade_Button")?.GetComponent<Button>();
+        LevelButton = transform.Find("Top/Level")?.GetComponent<Button>();
+
+        //EntryButton.onClick.AddListener(() => EntryPopUp.SetActive(true));
+        CodexButton.onClick.AddListener(()=> DonutCodex.SetActive(true));
+        //basketButton.onClick.AddListener(() => BasketPopUp.SetActive(true));
+        UpgradeButton.onClick.AddListener(()=> DonutUpgrade.SetActive(true));
+        LevelButton.onClick.AddListener(() => PlayerLevelInfo.SetActive(true));
+    }
+    void Start()
+    {
+        
+    }
+
+}
