cc2789acae467657e2abef2487e8ed287445bbb2
yybbuu1 <ybu1357@gmail.com>
2025-11-06 16:17:49 +0900

    [add] 엔트리팝업 UI제작 및 엔트리팝업스크립트 추가(최빛님과 머지후 기능 테스트)
    [fix]UImanager 팝업 닫기코드 추가
    [fix]menupanelcontrol 엔트리팝업관련 코드 추가

---
+++ Assets/_Project/Scripts/UI/Scr_EntryPopUP.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scr_EntryPopUP : MonoBehaviour
+{
+    [Header("닫기 버튼")]
+    [SerializeField] private Button CloseButton;
+
+
+
+     void Awake()
+    {
+        CloseButton = transform.Find("CloseButton")?.GetComponent<Button>();
+    }
+    void Start()
+    {
+        CloseButton.onClick.AddListener (() => 
+        {
+            UIManager.Instance.Close(PanelId.EntryPopUp);
+        });
+    }
+}
--- Assets/_Project/Scripts/UI/Scr_MenuPanelControl.cs
+++ Assets/_Project/Scripts/UI/Scr_MenuPanelControl.cs
@@ -19,11 +19,14 @@ public class Scr_PanelControl : MonoBehaviour
     [SerializeField] private GameObject donutCodexClickPopup;
     [SerializeField] private GameObject rewardCheckPopup;
     [SerializeField] private GameObject nickNameChangePopup;
+    [SerializeField] private GameObject EntryPopUp;
     
     [Header("화면 전환 버튼")]
     [SerializeField] private Button playerLevelInfoButton;
     [SerializeField] private Button donutCodexButton;
     [SerializeField] private Button donutUpgradeButton;
+    [SerializeField] private Button EntryPopUpButton;
+    [SerializeField] private Button detailedSettingsButton;
     
     void Awake()
     {
@@ -35,11 +38,13 @@ public class Scr_PanelControl : MonoBehaviour
         UIManager.Instance.RegisterPanel(PanelId.PlayerLevelInfoPopup,playerLevelInfoPopup);
         UIManager.Instance.RegisterPanel(PanelId.DonutCodexPopup,donutCodexPopup);
         UIManager.Instance.RegisterPanel(PanelId.DonutUpgradePopup,donutUpgradePopup);
-        
+        UIManager.Instance.RegisterPanel(PanelId.EntryPopUp, EntryPopUp);
         
         playerLevelInfoButton.onClick.AddListener(()=>UIManager.Instance.Open(PanelId.PlayerLevelInfoPopup));
         donutCodexButton.onClick.AddListener(()=>UIManager.Instance.Open(PanelId.DonutCodexPopup));
         donutUpgradeButton.onClick.AddListener(()=>UIManager.Instance.Open(PanelId.DonutUpgradePopup));
+        EntryPopUpButton.onClick.AddListener(() => UIManager.Instance.Open(PanelId.EntryPopUp));
+        detailedSettingsButton.onClick.AddListener(() => UIManager.Instance.Open(PanelId.DetailedSettingsPanel));
     }
 
 
