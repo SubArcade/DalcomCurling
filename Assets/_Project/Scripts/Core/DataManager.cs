22adf6549fe7786424fb95064cbae330a89d1659
Huroad <cabin9999@naver.com>
2025-11-07 12:06:28 +0900

    [feat] data 관리 수정 중

--- Assets/_Project/Scripts/Core/DataManager.cs
+++ Assets/_Project/Scripts/Core/DataManager.cs
@@ -1,33 +1,8 @@
 ﻿using System;
-using Firebase;
-using Firebase.Auth;
 using Firebase.Firestore;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using UnityEngine;
-
-[System.Serializable, FirestoreData]
-public class PlayerData // DB클래스
-{
-    [field: SerializeField, Tooltip("이메일")] [FirestoreProperty] public string email { get; set; }
-    [field: SerializeField, Tooltip("이메일")] [FirestoreProperty] public string nickname { get; set; }
-    [field: SerializeField, Tooltip("골드")] [FirestoreProperty] public int gold { get; set; }
-    [field: SerializeField, Tooltip("잼")] [FirestoreProperty] public int gem { get; set; }
-    [field: SerializeField, Tooltip("에너지")] [FirestoreProperty] public int energy { get; set; }
-    [field: SerializeField, Tooltip("레벨")] [FirestoreProperty] public int level { get; set; }
-    [field: SerializeField, Tooltip("경험치")] [FirestoreProperty] public int exp { get; set; }
-    [field: SerializeField, Tooltip("처음 접속 시간")] [FirestoreProperty] public Timestamp createAt { get; set; }
-    [field: SerializeField, Tooltip("마지막 접속 시간")] [FirestoreProperty] public long lastAt { get; set; }
-    
-    // 랭크 데이터
-    [field: SerializeField, Tooltip("솔로 점수")] [FirestoreProperty] public int soloScore { get; set; }
-    [field: SerializeField, Tooltip("솔로 티어")] [FirestoreProperty] public GameTier solotTier { get; set; }
-    
-    // 로컬 플레이어 데이터 (디비에 들어가지 않는 변수)
-    [field: SerializeField, Tooltip("최대 에너지")] public int maxEnergy { get; set; }
-    [field: SerializeField, Tooltip("충전 시간(초)")] public int perSecEnergy { get; set; }
-    
-}
 
 public enum GameMode
 {
@@ -58,23 +33,8 @@ public class DataManager : MonoBehaviour
     private string userCollection = "user";   // 컬랙션(테이블) 이름
     [Header("플레이어 데이터 (신규 생성 시 초기 저장, 기존 계정은 불러와 갱신)")]
     [SerializeField, Tooltip("프리미어키(PK)")] private string docId = "ZflvQAZZmYj1SKj8mZKZ";
-    
-    [SerializeField]
-    private PlayerData playerData = new PlayerData
-    {
-        email = "test@test.com",
-        nickname = "test",
-        gold = 250,
-        gem = 7,
-        energy = 10,
-        level = 1,
-        exp = 0,
-        lastAt = 0,
-        maxEnergy = 20,
-        perSecEnergy = 10,
-        soloScore = 0,
-        solotTier = GameTier.Bronze,
-    };
+
+    [SerializeField] private PlayerData playerData = new PlayerData();
     
     public PlayerData PlayerData => playerData;
     
