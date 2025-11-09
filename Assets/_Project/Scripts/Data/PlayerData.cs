62146443894c0c4bb21adff64acb5e69a8c9b533
Huroad <cabin9999@naver.com>
2025-11-09 15:20:42 +0900

    [faet] 데이서 셋 수정 (4개의 클래스)

--- Assets/_Project/Scripts/Data/PlayerData.cs
+++ Assets/_Project/Scripts/Data/PlayerData.cs
@@ -16,25 +16,25 @@ public class PlayerData
 
     // 랭크 데이터
     [field: SerializeField, Tooltip("솔로 점수")] [FirestoreProperty] public int soloScore { get; set; }
-    [field: SerializeField, Tooltip("솔로 티어")] [FirestoreProperty] public GameTier solotTier { get; set; }
+    [field: SerializeField, Tooltip("솔로 티어")] [FirestoreProperty] public GameTier soloTier { get; set; }
 
     // 로컬 플레이어 데이터 (디비에 들어가지 않는 변수)
     [field: SerializeField, Tooltip("최대 에너지")] public int maxEnergy { get; set; }
     [field: SerializeField, Tooltip("충전 시간(초)")] public int perSecEnergy { get; set; }
     
-    public PlayerData()
-    {
-        email = "test@test.com";
-        nickname = "test";
-        gold = 250;
-        gem = 7;
-        energy = 10;
-        level = 1;
-        exp = 0;
-        lastAt = 0;
-        maxEnergy = 20;
-        perSecEnergy = 10;
-        soloScore = 0;
-        solotTier = GameTier.Bronze;
-    }
+    // public PlayerData()
+    // {
+    //     email = "test@test.com";
+    //     nickname = "test";
+    //     gold = 250;
+    //     gem = 7;
+    //     energy = 10;
+    //     level = 1;
+    //     exp = 0;
+    //     lastAt = 0;
+    //     maxEnergy = 20;
+    //     perSecEnergy = 10;
+    //     soloScore = 0;
+    //     soloTier = GameTier.Bronze;
+    // }
 }
