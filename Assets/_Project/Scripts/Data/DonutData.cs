8a6c91890031701338d991a0a5c01390fb445ba9
tuckinchu <aple34@naver.com>
2025-11-10 14:19:52 +0900

    [feat] 도넛레벨과 수치에 따른 도넛생성
    도넛에 들어가는 수치에 따라서 도형의 외형이 결정되고 도넛레벨에 따라서 도넛의 외형이 변경됨

--- Assets/_Project/Scripts/Data/DonutData.cs
+++ Assets/_Project/Scripts/Data/DonutData.cs
@@ -1,18 +1,31 @@
+﻿using Firebase.Firestore;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+[System.Serializable, FirestoreData]
 
-public class DonutData : MonoBehaviour
+public enum DonutType 
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    Hard, //단단
+    Soft, //말랑
+    Moist //촉촉
+}
+public class DonutData
+{
+    // 도넛 종류 넣기
+    [field: SerializeField, Tooltip("무게")][FirestoreProperty] public int Weight { get; set; }
+    [field: SerializeField, Tooltip("반발력")][FirestoreProperty] public int Resilience { get; set; }
+    [field: SerializeField, Tooltip("마찰력")][FirestoreProperty] public int Friction { get; set; }
+    //인게임에 진입할때 호출할 데이터
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [field: SerializeField, Tooltip("도넛레벨")][FirestoreProperty] private string Hard01 { get; set; }
+    //메인메뉴에서 정보를 호출하기위한 키
+
+
+
+    // 1 도넛 이름 -            2 도넛 종류                 
+    // name= StarDonut 01~30 /  type = hard , soft, moist/
+    //  ㄴ 스펙 마찰 무게 반발력
+
+
 }
