749d3f1fc269c6512470346b2ffbcca8cc5bbaad
Huroad <cabin9999@naver.com>
2025-11-11 16:59:17 +0900

    [feat, update] 데이터 셋 통합 정리 완료, 도넛 SO 정리 완료, 데이터 매니저 SO값 대입 완료

---
+++ Assets/_Project/Scripts/Data/QuestData.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using Firebase.Firestore;
+using UnityEngine;
+
+[System.Serializable, FirestoreData]
+public class QuestList
+{
+    [Tooltip("도넛 고유 아이디")] [FirestoreProperty] public string donutid { get; set; }    
+    [Tooltip("새로고침 횟수")] [FirestoreProperty] public int refreshCount { get; set; }
+    [Tooltip("보상 골드")] [FirestoreProperty] public int rewardGold { get; set; }
+}
+
+[System.Serializable, FirestoreData]
+public class QuestData
+{
+    [field: SerializeField, Tooltip("도넛 앤트리")][FirestoreProperty] public List<QuestList> questList { get; set; } = new();
+    
+    // 로컬 퀘스트 데이터 (디비에 들어가지 않는 변수)
+    public int baseGold { get; set; }
+}
