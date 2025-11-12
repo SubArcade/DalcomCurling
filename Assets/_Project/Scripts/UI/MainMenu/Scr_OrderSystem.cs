d8f3765fe8d4fbde51dde78ad0813cccbfb12fb3
yybbuu1 <ybu1357@gmail.com>
2025-11-12 11:59:26 +0900

    [update]주문서 스크립트 수정중 최빛님과 머지후 작업, 도넛SO들 이미지 설명 기입, 프리팹연결 필요

--- Assets/_Project/Scripts/UI/MainMenu/Scr_OrderSystem.cs
+++ Assets/_Project/Scripts/UI/MainMenu/Scr_OrderSystem.cs
@@ -34,16 +34,12 @@ public class Scr_OrderSystem : MonoBehaviour
     [Header("갱신된 새로고침 횟수 텍스트")]
     [SerializeField] private TextMeshProUGUI RefreshCountText;
 
-    [Header("주문서에 나타날 무작위 도넛들")] 
-    [SerializeField] private List<GameObject> DonutList;
-
-    private BoardManager boardManager; //머지판에 담긴 도넛 ID 호출을 위한 변수
 
     private int RefreshCount = 5; //새로고침 횟수
     private const int MaxRefreshCount = 5; //최대 새로고침 횟수
 
     // 도넛이미지와 같이 갱신될 도넛타입TextMeshPro, 도넛단계 TextMeshPro
-    private class DonutTextInfo
+    public class DonutTextInfo
     {
         public TextMeshProUGUI LevelText;
         public TextMeshProUGUI TypeText;
@@ -52,7 +48,11 @@ public class Scr_OrderSystem : MonoBehaviour
     private List<DonutTextInfo> DonutTextInfos1 = new List<DonutTextInfo>();
     private List<DonutTextInfo> DonutTextInfos2 = new List<DonutTextInfo>();
     private List<DonutTextInfo> DonutTextInfos3 = new List<DonutTextInfo>();
-
+    
+    // 주문서별 도넛 ID 저장 리스트 추가
+    private List<string> OrderDonutIDs1 = new List<string>();
+    private List<string> OrderDonutIDs2 = new List<string>();
+    private List<string> OrderDonutIDs3 = new List<string>();
 
     void Awake()
     {
@@ -80,12 +80,11 @@ public class Scr_OrderSystem : MonoBehaviour
         CompleteObject2 = transform.Find("Top/QuestGroup/Order2/CompleteObject")?.gameObject;
         CompleteObject3 = transform.Find("Top/QuestGroup/Order3/CompleteObject")?.gameObject;
 
-        boardManager = BoardManager.Instance;
-
         // orderdonuts1~3의 자식 텍스트들을 리스트에 저장
         DonutTextInfos1 = ExtractTextInfos(OrderDonuts1);
         DonutTextInfos2 = ExtractTextInfos(OrderDonuts2);
-        DonutTextInfos3 = ExtractTextInfos(OrderDonuts3);      
+        DonutTextInfos3 = ExtractTextInfos(OrderDonuts3);
+
     }
     void Start()
     {     
@@ -95,23 +94,21 @@ public class Scr_OrderSystem : MonoBehaviour
         RefreshBtn3.onClick.AddListener(() => OnclickRefreshOrderButton(RefreshBtn3));
 
         //주문서 완료 연결
-        OrderClearBtn1.onClick.AddListener(() => OnClickClearButton(CompleteObject1));
-        OrderClearBtn2.onClick.AddListener(() => OnClickClearButton(CompleteObject2));
-        OrderClearBtn3.onClick.AddListener(() => OnClickClearButton(CompleteObject3));
+        OrderClearBtn1.onClick.AddListener(() => OnClickClearButton(CompleteObject1, OrderDonutIDs1));
+        OrderClearBtn2.onClick.AddListener(() => OnClickClearButton(CompleteObject2, OrderDonutIDs2));
+        OrderClearBtn3.onClick.AddListener(() => OnClickClearButton(CompleteObject3, OrderDonutIDs3));
 
     }
 
     //새로고침 버튼 상호작용
-    public void OnclickRefreshOrderButton(Button ClickedButton) 
-    {      
-        if (RefreshCount <= 0) 
+    //새로고침 버튼 상호작용
+    public void OnclickRefreshOrderButton(Button ClickedButton)
+    {
+        if (RefreshCount <= 0)
         {
             print("남은 새로고침 횟수가 없습니다!");
             return;
         }
-
-        //게임 시작할때 반드시 1번은 새로고침을 하되 횟수는 빼지않기
-        //리롤하면 도넛 이미지 + 도넛에 붙은 레벨,타입텍스트 랜덤으로 넣어서 갱신해야함
 
         RefreshCount--;
         RefreshCountText.text = $"{RefreshCount}/{MaxRefreshCount}";
@@ -119,19 +116,21 @@ public class Scr_OrderSystem : MonoBehaviour
         // 해당 주문서의 CompleteObject 비활성화
         if (ClickedButton == RefreshBtn1)
         {
+            RefreshOrderDonut(OrderDonuts1, DonutTextInfos1, OrderDonutIDs1);
             CompleteObject1.SetActive(false);
         }
         else if (ClickedButton == RefreshBtn2)
-        { 
-            CompleteObject2.SetActive(false); 
+        {
+            RefreshOrderDonut(OrderDonuts2, DonutTextInfos2, OrderDonutIDs2);
+
+            CompleteObject2.SetActive(false);
         }
         else if (ClickedButton == RefreshBtn3)
         {
-            CompleteObject3.SetActive(false); 
+            RefreshOrderDonut(OrderDonuts3, DonutTextInfos3, OrderDonutIDs3);
+            CompleteObject3.SetActive(false);
         }
     }
-
-
 
     //orderdonuts1~3에서 자식텍스트를 찾아주는 함수
     private List<DonutTextInfo> ExtractTextInfos(List<Image> donutImages)
@@ -142,8 +141,8 @@ public class Scr_OrderSystem : MonoBehaviour
         {
             var info = new DonutTextInfo
             {
-                TypeText = img.transform.Find("Type_text")?.GetComponent<TextMeshProUGUI>(),
-                LevelText = img.transform.Find("Level_text")?.GetComponent<TextMeshProUGUI>()
+                TypeText = img.transform.Find("Type_Text")?.GetComponent<TextMeshProUGUI>(),
+                LevelText = img.transform.Find("Level_Text")?.GetComponent<TextMeshProUGUI>()
             };
 
             result.Add(info);
@@ -152,8 +151,47 @@ public class Scr_OrderSystem : MonoBehaviour
         return result;
     }
 
+    //주문서 새로고침시 이미지, 이름.레벨 갱신
+    public void RefreshOrderDonut(List<Image> orderImages, List<DonutTextInfo> textInfos, List<string> idList)
+    {
+        idList.Clear();
+        int count = Mathf.Min(orderImages.Count, textInfos.Count, 3);
+
+        for (int i = 0; i < count; i++)
+        {
+            DonutType randomType = (DonutType)Random.Range(0, System.Enum.GetValues(typeof(DonutType)).Length);
+            int randomLevel = Random.Range(1, 31);
+
+            DonutData donut = DataManager.Instance.GetDonutData(randomType, randomLevel);
+            
+            orderImages[i].sprite = donut.sprite;
+            textInfos[i].TypeText.text = donut.displayName;
+            textInfos[i].LevelText.text = donut.description;
+
+            idList.Add(donut.id);
+        }
+    }
+    //머지보드판에서 도넛 검색후 주문서 클리어 여부 결정
+    public bool IsOrderCompleted(List<string> orderDonutIDs)
+    {
+        var boardCells = BoardManager.Instance.GetAllCells();
+
+        HashSet<string> boardDonutIDs = new();
+        foreach (var cell in boardCells)
+        {
+            if (!cell.isActive || string.IsNullOrEmpty(cell.donutID)) continue;
+            boardDonutIDs.Add(cell.donutID);
+        }
+
+        foreach (var id in orderDonutIDs)
+        {
+            if (!boardDonutIDs.Contains(id))
+                return false;
+        }
+        return true;
+    }
     //주문서 완료시 버튼 상호작용
-    public void OnClickClearButton(GameObject completeObject)
+    public void OnClickClearButton(GameObject completeObject, List<string> orderDonutIDs)
     {
         //해당하는 보상 수령 로직도 넣기
         //ex)단단1단계 200~250 + 말랑2단계 225~275 + 촉촉 3단계 250~275 = 보상골드
