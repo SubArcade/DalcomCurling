1d1cc1deafebbcb3af96b9598082dc21c3eb7951
yybbuu1 <ybu1357@gmail.com>
2025-11-18 16:11:56 +0900

    [add]아래 내용도 확인해주세요 매칭팝업창 취소 누를시 취소기능 스크립트추가
    texts.json 수정
    ordersystem.cs 코드 수정(데이터전달방식 수정, 주문서에 기프트박스 나오지않도록 수정, 주문서 로컬라이징 적용)
    localizedtexts.cs 코드 추가(비활성화여도 업데이트 되도록)
    폴더 YBUprefab에 UI 수정된것들 모아놨습니다 UIfrefab이랑 헷갈릴까봐 넣었습니다
    donutupgradepopup.cs 수정(로컬라이징 적용)

---
+++ Assets/_Project/Scripts/UI/Scr_MatchingPopUp.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scr_MatchingPopUp : MonoBehaviour
+{
+    [SerializeField] private Button cancleButton;
+
+    void Awake()
+    {
+        cancleButton = transform.Find("background/CancleButton")?.GetComponent<Button>();
+    }
+    void Start()
+    {
+        cancleButton.onClick.AddListener(() =>
+        {
+            FirebaseMatchmakingManager.Instance.CancelMatchmaking();
+            UIManager.Instance.Close(PanelId.MatchingPopUp);
+        });
+    }
+}
