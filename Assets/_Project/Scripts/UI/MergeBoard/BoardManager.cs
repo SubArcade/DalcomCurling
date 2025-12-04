3c065377a2f4794f992002867dd4c559eca2dcd7
tuckinchu <aple34@naver.com>
2025-12-04 14:57:51 +0900

    [feat] 생성기쪽 칸 선택시 나오는 사운드 문제 해결

--- Assets/_Project/Scripts/UI/MergeBoard/BoardManager.cs
+++ Assets/_Project/Scripts/UI/MergeBoard/BoardManager.cs
@@ -187,6 +187,7 @@ public class BoardManager : MonoBehaviour
         selectionHighlight.transform.SetParent(cell.transform, false);
         selectionHighlight.rectTransform.anchoredPosition = Vector2.zero;
         // 칸 선택 사운드
+        if (generatorCell) return;
         SoundManager.Instance.selectSlotScroll();
     }
 
